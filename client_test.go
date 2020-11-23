e0690f0d43c0bb0c74503a81045dce9164cdbcf4
Gustavo Chain <gchain@pm.me>
2020-11-23 10:07:05 +0100

    skip integration test when no env

--- client_test.go
+++ client_test.go
@@ -1,23 +1,56 @@
 package etebase
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	envAccount  = "ETEBASE_TEST_ACCOUNT"
+	envUsername = "ETEBASE_TEST_USERNAME"
+	envEmail    = "ETEBASE_TEST_EMAIL"
+	envPassword = "ETEBASE_TEST_PASSWORD"
+)
+
+func envSkip(t *testing.T, env string) {
+	t.Skipf("Define %s to run this test", env)
+}
+
+func testUserFromEnv(t *testing.T) (User, string) {
+	user, pass := User{
+		Username: os.Getenv(envUsername),
+		Email:    os.Getenv(envEmail),
+	}, os.Getenv(envPassword)
+
+	if user.Username == "" {
+		envSkip(t, envUsername)
+	}
+
+	if user.Email == "" {
+		envSkip(t, envEmail)
+	}
+
+	if pass == "" {
+		envSkip(t, envPassword)
+	}
+
+	return user, pass
+}
+
 func TestClient(t *testing.T) {
+	accountName := os.Getenv(envAccount)
+	if accountName == "" {
+		envSkip(t, envAccount)
+	}
+	user, password := testUserFromEnv(t)
+
 	var (
 		acc = NewAccount(
-			NewClient("gchaincl", DefaultClientOptions),
+			NewClient(accountName, DefaultClientOptions),
 		)
-
-		user = User{
-			Username: "gchaincl",
-			Email:    "gchain@pm.me",
-		}
-		password = "foo"
 	)
 
 	t.Run("Signup", func(t *testing.T) {
