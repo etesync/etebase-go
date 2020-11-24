cd1cc259a121c1cebf0704984207dc8adc021460
Gustavo Chain <gchain@pm.me>
2020-11-24 14:58:56 +0100

    Encoding: remove JSON encoding support
    We were sending msgpack encoded request and receiving JSON encoded
    responses from the server.
    
    This PR adds the 'Accept: application/msgpack' HTTP header to receive
    msgpack encoded responses.

---
+++ codec.go
@@ -0,0 +1,19 @@
+package etebase
+
+import (
+	"io"
+
+	"github.com/vmihailenco/msgpack"
+)
+
+type Decoder interface {
+	Decode(interface{}) error
+}
+
+func NewDecoder(r io.Reader) Decoder {
+	return msgpack.NewDecoder(r)
+}
+
+func Marshal(v interface{}) ([]byte, error) {
+	return msgpack.Marshal(v)
+}
