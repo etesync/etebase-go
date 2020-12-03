712b3694c82e23b56db304f411618ec338f23ee9
Gustavo Chain <gchain@pm.me>
2020-12-03 13:27:05 +0100

    wip

--- scripts/wait-for-it.sh
+++ scripts/wait-for-it.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-# Use this script to test if a given TCP host/port are available
 
+# SPDX-FileCopyrightText: © 2017  vishnubob/wait-for-it  Authors
+# SPDX-License-Identifier: MIT
+
+# Use this script to test if a given TCP host/port are available
 WAITFORIT_cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
