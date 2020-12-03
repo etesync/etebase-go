3af54709d48ec5eda915ca393e116e88ec530fcc
Gustavo Chain <gchain@pm.me>
2020-12-03 13:47:14 +0100

    licence: licence all files

--- scripts/testmode.sh
+++ scripts/testmode.sh
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2020 Etebase Authors
+# SPDX-License-Identifier: BSD-3-Clause
+
 # testmode.sh is used to make :victorrds/etebase suitable to testing.
 # It enables the user signup via API
 sed -e '/ETEBASE_CREATE_USER_FUNC/ s/^#*/#/' -i etebase_server/settings.py
