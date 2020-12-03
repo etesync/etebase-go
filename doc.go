0d35265f1f415e66b7a20d324b45ed247cce0ea0
Gustavo Chain <gchain@pm.me>
2020-12-03 13:47:14 +0100

    licence: licence all files

--- doc.go
+++ doc.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: © 2020 Etebase Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
 // package etebase implements a Etebase client.
 // Etebase is an end-to-end encrypted backend as a service.
 // Think Firebase, but encrypted in a way that only your users can access their data.
